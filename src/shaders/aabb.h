120358c924fb6747ec125c6e93ff08c41613d901
Jose Esteve <jose@joesfer.com>
2015-01-25 11:43:09 +0000

    Allow for traversal to start within the volume (so we can move the camera inside).

--- src/shaders/aabb.h
+++ src/shaders/aabb.h
@@ -6,9 +6,9 @@ float rayAABBIntersection(in vec3 o, in vec3 d,
 	vec3 tMin3 = (boundsMin - o) * invDir;
 	vec3 tMax3 = (boundsMax - o) * invDir;
 
-	vec3 tmin = min(tMin3, tMax3);
+	vec3 tmin   = min(tMin3, tMax3);
 	float tminf = max(tmin.x, max(tmin.y, tmin.z));
-	vec3 tmax = max(tMin3, tMax3);
+	vec3 tmax   = max(tMin3, tMax3);
 	float tmaxf = min(tmax.x, min(tmax.y, tmax.z));
 
 	// if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behind us
@@ -23,7 +23,11 @@ float rayAABBIntersection(in vec3 o, in vec3 d,
 		return -1; 
 	}
 
-	return tminf >= 0 ? tminf : tmaxf;
+	// this is a bit of a specific behaviour to raymarching: if we're inside of 
+	// the volume we're not interested in the outgoing (tmax) intersection, but
+	// simply clamp tmin to 0 to start marching from the current ray origin.
+	return max(0, tminf); 
+	// this would be the usual ray/aabb intersection code
+	//return tminf >= 0 ? tminf : tmaxf;
 }
-
 
